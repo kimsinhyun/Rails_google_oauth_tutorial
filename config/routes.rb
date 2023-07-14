22637ed899ce2bcf9b6b05ccbfbae78a98831b52
sinhyun Kim <ksh415622@naver.com>
2023-07-14 19:33:06 +0900

    chore: add comment to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
+  # @todo: gcp console 에서 oauth uri 설정을 해야함 (우선은 /admin 으로 해뒀음, 추후 /user 로 변경)
   devise_for :users,
              path: '/admin',
              path_names: {
